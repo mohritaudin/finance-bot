29baece04711ca70a89032ef2efa66ddf3874cd6
mohritaudin <99479425+mohritaudin@users.noreply.github.com>
2026-04-22 10:01:08 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -56,7 +56,7 @@ def retry(func, retries=3, delay=2):
 genai.configure(api_key=GEMINI_API_KEY)
 
 # pakai model stabil
-model = genai.GenerativeModel("gemini-pro")
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 # ===== GOOGLE SHEETS =====
 def get_sheet():
